e701b464f3e4d8dd8a089eff22482cd742cd64a8
RahulK2202 <rahuljcet95@gmail.com>
2023-08-04 10:30:32 +0530

    url fixed erros

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -1,3 +1,5 @@
 
 
-export const BACKEND_BASE_URL = process.env.REACT_APP_BACKEND_BASE_URL 
+// export const BACKEND_BASE_URL = process.env.REACT_APP_BACKEND_BASE_URL 
+
+export const BACKEND_BASE_URL='https://officexpert.onrender.com'
