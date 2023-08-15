9f1d42de7772a516d8b4dbc8d5684cc5c8921971
RahulK2202 <rahuljcet95@gmail.com>
2023-08-15 15:00:35 +0530

    pagination search on annoucement

--- src/components/adminside/AnnoucementTable.jsx
+++ src/components/adminside/AnnoucementTable.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import Swal from 'sweetalert2';
-
 import Announcements from './Announcements';
-import { fetchAnnouncements, addAnnouncement,deleteAnnouncement } from '../../data/announcementApi';
+import { fetchAnnouncements, addAnnouncement, deleteAnnouncement } from '../../data/announcementApi';
+import Pagination from '../../Pagination/Pagination'; // Make sure to adjust the import path
 
 function AnnouncementTable() {
   const [announcements, setAnnouncements] = useState([]);
-
-
+  const [currentPage, setCurrentPage] = useState(0);
+  const [searchQuery, setSearchQuery] = useState('');
+  const itemsPerPage = 5;
 
   const fetchAnnouncementData = async () => {
     try {
@@ -22,7 +23,6 @@ function AnnouncementTable() {
     fetchAnnouncementData();
   }, []);
 
-  
   const handleAddAnnouncement = async (newAnnouncement) => {
     try {
       const createdAnnouncement = await addAnnouncement(newAnnouncement);
@@ -32,14 +32,8 @@ function AnnouncementTable() {
     }
   };
 
- 
-  
-
-
-
-  const handleDeleteAnnouncement= async (announcementId) => {
+  const handleDeleteAnnouncement = async (announcementId) => {
     try {
-      // Show a confirmation dialog
       const result = await Swal.fire({
         title: 'Do you really want to delete this announcement?',
         icon: 'warning',
@@ -47,7 +41,7 @@ function AnnouncementTable() {
         confirmButtonText: 'Yes, delete it!',
         cancelButtonText: 'No, cancel',
       });
-  
+
       if (result.isConfirmed) {
         await deleteAnnouncement(announcementId);
         console.log('Announcement deleted successfully');
@@ -59,12 +53,25 @@ function AnnouncementTable() {
       console.error('Error deleting announcement:', error);
     }
   };
-  
 
+  const handlePageChange = ({ selected }) => {
+    setCurrentPage(selected);
+  };
 
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+    setCurrentPage(0);
+  };
 
+  const filteredAnnouncements = announcements.filter((announcement) =>
+    announcement.event.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
-
+  const pageCount = Math.ceil(filteredAnnouncements.length / itemsPerPage);
+  const displayedAnnouncements = filteredAnnouncements.slice(
+    currentPage * itemsPerPage,
+    (currentPage + 1) * itemsPerPage
+  );
 
   return (
     <div>
@@ -73,49 +80,47 @@ function AnnouncementTable() {
           <div className="bg-gray-200 px-2 py-3 border-solid border-gray-200 border-b">
             Announcement Lists
           </div>
-          <td>
-            <br />
-            {/* <Announcements onAdd={addAnnouncement} /> */}
-            <Announcements onAdd={handleAddAnnouncement} />
-
-          </td>
-          <br />
-
-
-
+          <div className="mb-4 mt-2 px-2">
+            <input
+              type="text"
+              placeholder="Search by Event"
+              value={searchQuery}
+              onChange={handleSearch}
+              className="px-4 py-2 border rounded w-full"
+            />
+          </div>
           <div className="p-3">
-  <table className="table-responsive w-full rounded">
-    <thead>
-      <tr>
-        <th className="border px-4 py-2">Id</th>
-        <th className="border w-1/2 px-4 py-2">Event</th>
-        <th className="border w-1/2 px-4 py-2">Notes</th>
-        <th className="border w-1/2 px-4 py-2">Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      {announcements.map((announcement) => (
-        <tr key={announcement.id}>
-          <td className="border px-4 py-2">{announcement.id}</td>
-          <td className="border px-4 py-2">{announcement.event}</td>
-          <td className="border px-4 py-2">{announcement.note}</td>
-          <td className="border px-4 py-2">
-            <button
-              className="bg-teal-300 cursor-pointer rounded p-1 mx-1 text-red-500"
-              onClick={() => handleDeleteAnnouncement(announcement.id)}
-            >
-              <i className="fas fa-trash"></i>
-            </button>
-          </td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-</div>
-
-
-
-
+            <table className="table-responsive w-full">
+              <thead>
+                <tr>
+                  <th className="border px-4 py-2">Id</th>
+                  <th className="border w-1/2 px-4 py-2">Event</th>
+                  <th className="border w-1/2 px-4 py-2">Notes</th>
+                  <th className="border w-1/2 px-4 py-2">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {displayedAnnouncements.map((announcement) => (
+                  <tr key={announcement.id}>
+                    <td className="border px-4 py-2">{announcement.id}</td>
+                    <td className="border px-4 py-2">{announcement.event}</td>
+                    <td className="border px-4 py-2">{announcement.note}</td>
+                    <td className="border px-4 py-2">
+                      <button
+                        className="bg-teal-300 cursor-pointer rounded p-1 mx-1 text-red-500"
+                        onClick={() => handleDeleteAnnouncement(announcement.id)}
+                      >
+                        <i className="fas fa-trash"></i>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="flex justify-center mt-4">
+            <Pagination pageCount={pageCount} onPageChange={handlePageChange} />
+          </div>
         </div>
       </div>
     </div>
